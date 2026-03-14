bf2ab3ef145d91e2658e32adaa1de012356953a4
Trisha Bharti <trishabharti444@gmail.com>
2026-03-14 17:40:55 +0530

    Initial Commit

---
+++ block-Game/block_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  block_GameApp.swift
+//  block-Game
+//
+//  Created by Vikram Kumar on 14/03/26.
+//
+
+import SwiftUI
+
+@main
+struct block_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
