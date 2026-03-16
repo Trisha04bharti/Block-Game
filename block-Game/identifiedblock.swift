642a43e7a3e220d649064b292d1f756c7cb424ee
Trisha Bharti <trishabharti444@gmail.com>
2026-03-16 19:20:14 +0530

    added model

---
+++ block-Game/identifiedblock.swift
@@ -0,0 +1,15 @@
+//
+//  identifiedblock.swift
+//  block-Game
+//
+//  Created by Vikram Kumar on 16/03/26.
+//
+
+import Foundation
+
+struct IdentifiedBlock: Block {
+    
+    let id: Int
+    var number: Int
+    
+}
